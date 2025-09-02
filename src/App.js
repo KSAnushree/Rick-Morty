7edef45ae7a6bcbe117d118f7af30b00bef73444
KSAnushree <anushreetutu2006@gmai.com>
2025-09-03 00:15:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Web from "./components/Web";
+
+function App() {
+  return (
+    <div className="App">
+      <Web />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Web.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Web = () => {
+  return (
+    <div>
+      <h1 className="text-white font-bold text-5xl p-8 underline ">
+        The Rick and Morty
+      </h1>
+    </div>
+  );
+};
+
+export default Web;
